a1f5dbf7f1dcaf2e3dd6b87aa5ea2b35e6408950
Gustavo <gustavovilleth@gmail.com>
2024-09-27 16:23:28 -0300

    att projeto

--- screens/Login.js
+++ screens/Login.js
@@ -1,46 +1,138 @@
-import react, { useState } from 'react';
-import { Button, StyleSheet, View } from 'react-native';
-import { Input, Text } from 'react-native-elements';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from 'react';
+import { Alert, StyleSheet, View } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+import { ActivityIndicator } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import usuarioService from '../services/UsuarioService';
 import styles from '../style/MainStyle';
 
 
 export default function Login({navigation}) {
-  
+
   const [email, setEmail] = useState(null)
   const [password, setPassword] = useState(null)
-  
+  const [isLoading, setLoading] = useState(false)
+  const [isLoadingToken, setLoadingToken] = useState(true)
+
   const entrar = () => {
-    navigation.reset({
+
+    let data = {
+      username: email,
+      password: password
+    }
+    
+    usuarioService.login(data)
+    .then((response) => {
+      setLoading(false)
+      navigation.reset({
         index: 0,
         routes: [{name: "Principal"}]
+      })
+    })
+    .catch((error) => {
+      setLoading(false)
+      Alert.alert("Usuário não existe")
     })
   }
-  
-  
+
+  const logarComToken = (token) => {
+
+    setLoadingToken(true)
+    let data = {
+      token: token
+    }
+    
+    usuarioService.loginComToken(data)
+    .then((response) => {
+      setLoadingToken(false)
+      navigation.reset({
+        index: 0,
+        routes: [{name: "Principal"}]
+      })
+    })
+    .catch((error) => {
+      setLoadingToken(false)      
+    })
+  }
+
+  const cadastrar = () => {
+    navigation.navigate("Cadastro")
+  }
+
+  useEffect(() => {
+    AsyncStorage.getItem("TOKEN").then((token) => {
+      logarComToken(token)
+    })
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text h3>Entre no APP</Text>
-      <Input 
-       placeholder="E-mail"
-       leftIcon={{ type: 'font-awsome' , name: 'person' }}
-       onChangeText={value => setEmail(value)}
-       keyboardType='email-address'
-      />
-      <Input 
-       placeholder="Sua senha"
-       leftIcon={{ type: 'font-awsome' , name: 'lock' }}
-       onChangeText={value => setPassword(value)}
-       secureTextEntry={true}
-      />
-      <Button
-        title="Entrar"
-        size={24}
-        color="#f194ff"
-        onPress={() => entrar()}
-      />
+    <View style={[styles.container, specificStyle.specificContainer]}>
+
+      { isLoadingToken && 
+        <Text>Só um minutinho...</Text> 
+      }
+
+      { !isLoadingToken && 
+        <>
+          <Text h3>Entre no app</Text>
+          <Input
+            placeholder="E-mail"
+            leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+            onChangeText={value => setEmail(value)}
+            keyboardType="email-address"
+            />
+          <Input
+            placeholder="Sua senha"
+            leftIcon={{ type: 'font-awesome', name: 'lock' }}
+            onChangeText={value => setPassword(value)}
+            secureTextEntry={true}
+            />
+          
+          { isLoading && 
+            <ActivityIndicator />
+          }
+
+          { !isLoading && 
+            <Button
+              icon={
+                <Icon
+                  name="check"
+                  size={15}
+                  color="white"
+                />
+              }
+              title="Entrar"
+              buttonStyle={specificStyle.button}
+              onPress={() => entrar()}
+            />
+          }
+
+          <Button
+            icon={
+              <Icon
+                name="user"
+                size={15}
+                color="white"
+              />
+            }
+            title=" Cadastrar"
+            buttonStyle={specificStyle.button}
+            onPress={() => cadastrar()}
+          />
+        </>
+      }
 
     </View>
   );
 }
 
-
+const specificStyle = StyleSheet.create({
+  specificContainer: {
+    backgroundColor: "#fff"
+  },
+  button: {
+    width: "100%",
+    marginTop: 10
+  }
+})
